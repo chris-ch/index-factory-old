c47eb58706ff240c467d78d4c8f0ca10b9c59d09
Christophe <ch.alexandre@bluewin.ch>
2020-01-26 18:06:43 +0100

    dev

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 import os
 import csv
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import logging
 import flask
 import decimal
+from datetime import date, datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -39,12 +41,24 @@ def hello():
     return "Hello World!"
 
 
+def make_index_partition_key(index_code):
+    return 'index#' + index_code
+
+def make_index_sort_key(index_code):
+    return 'index-details#' + index_code
+
+def make_prices_partition_key(market_code):
+    return 'prices#' + market_code
+
+def make_prices_sort_key(as_of_date: date):
+    return 'daily#' + as_of_date.strftime('%Y%m%d')
+
 @handler.route("/indices/<string:index_code>")
 def get_index(index_code):
-    indices = db.Table(INDEX_FACTORY_TABLE)
-    resp = indices.get_item(
+    table = db.Table(INDEX_FACTORY_TABLE)
+    resp = table.get_item(
         Key={
-            'indexCode': index_code
+            'partitionKey': make_index_partition_key(index_code)
         }
     )
     item = resp.get('Item')
@@ -63,10 +77,11 @@ def create_index():
     if not index_code or not name:
         return flask.jsonify({'error': 'Please provide indexCode and name'}), 400
 
-    indices = db.Table(INDEX_FACTORY_TABLE)
-    indices.put_item(
-        Item=flask.request.json
-    )
+    table = db.Table(INDEX_FACTORY_TABLE)
+    index_data = {key: value for key, value in flask.request.json.items()}
+    index_data['partitionKey'] = make_index_partition_key(index_code)
+    index_data['sortKey'] = make_index_sort_key(index_code)
+    table.put_item(Item=index_data)
 
     return flask.jsonify({
         'indexCode': index_code,
@@ -76,14 +91,22 @@ def create_index():
 
 @handler.route("/indices", methods=["GET"])
 def list_indices():
-    indices = db.Table(INDEX_FACTORY_TABLE)
-    results = indices.scan(Select='ALL_ATTRIBUTES')
+    table = db.Table(INDEX_FACTORY_TABLE)
+    results = table.scan(Select='ALL_ATTRIBUTES', FilterExpression=Key('partitionKey').begins_with('index#'))
     return flask.jsonify(results.get('Items'))
 
-# compute_index(code, as_of_date)
-# upload_prices(as_of_date, file)
-# upload_price(timestamp, value)  -- realtime indices
+@handler.route("/prices", methods=["GET"])
+def list_prices():
+    table = db.Table(INDEX_FACTORY_TABLE)
+    results = table.scan(Select='ALL_ATTRIBUTES', FilterExpression=Key('partitionKey').begins_with('prices#'))
+    data = [{key: value for key, value in row.items() if key != 'prices'} for row in results.get('Items')]
+    for row, item in zip(data, results.get('Items')):
+        row['count'] = len(item['prices'])
+        
+    return flask.jsonify(data)
 
+# compute_index(code, as_of_date)
+# upload_price(timestamp, value)  -- realtime indices
 
 @handler.route('/upload-prices/<string:market_code>', methods=['POST'])
 def upload_prices(market_code):
@@ -103,8 +126,23 @@ def upload_prices(market_code):
     content = [line.decode('utf-8').strip().split(',') for line in file.readlines()]
     header = content[0]
     prices = [dict(zip(header, row)) for row in content[1:]]
+    if len(prices) == 0:
+        return flask.jsonify({
+            'marketCode': market_code,
+            'header': header,
+            'count': 0
+            })
+
     logger.info('head prices: %s', str(prices[:5]))
     logger.info('tail prices: %s', str(prices[-5:]))
+    table = db.Table(INDEX_FACTORY_TABLE)
+    as_of_date = datetime.strptime(prices[0]['Date'], '%d-%b-%Y')
+    prices_data = {
+        'partitionKey': make_prices_partition_key(market_code),
+        'sortKey': make_prices_sort_key(as_of_date),
+        'prices': prices
+    }
+    table.put_item(Item=prices_data)
     return flask.jsonify({
         'marketCode': market_code,
         'header': header,
